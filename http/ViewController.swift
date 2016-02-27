370b0d181a6565f2ab268e3473270c8258dc4086
GrandreChina <Grandre@zhangyingdemacbook-air.local>
2016-02-27 11:44:18 +0800

    Initial Commit

---
+++ http/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  http
+//
+//  Created by Grandre on 16/2/27.
+//  Copyright © 2016年 革码者. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
